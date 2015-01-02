d10a11f506810bc8d2589c51d5789dd08eee7223
Darren Duncan <darren@DarrenDuncan.net>
2015-01-02 00:19:49 -0800

    change docs to ref Muldis::D::RefEng rather than Muldis Rosetta, which is obsolete

--- lib/Set/Relation.pm
+++ lib/Set/Relation.pm
@@ -79,7 +79,7 @@ I<This documentation is pending.>
 Set::Relation provides a simple Perl-native facility for an application to
 organize and process information using the relational model of data,
 without having to employ a separate DBMS, and without having to employ a
-whole separate sub-language (such as L<Muldis Rosetta|Muldis::Rosetta>
+whole separate sub-/language (such as L<Muldis::D::RefEng>
 does).  Rather, it is integrated a lot more into the Perl way of doing
 things, and you use it much like a Perl array or hash, or like some other
 third-party Set:: modules available for Perl.  This module defines a Perl 5
@@ -99,6 +99,10 @@ B<If you want to help out with this module's development, generally the
 most helpful thing you can do to start out is to flesh out the test suite.
 I suggest looking at the test suites of other Set:: modules as well as
 various database related modules as inspiration or a source for copying.>
+
+B<That being said, Set::Relation is several years out of date on its
+tracking of the Muldis D language, so there is limited value in spending a
+lot of time on it before Muldis::D::RefEng has nailed the spec down.>
 
 Loosely speaking, a Set::Relation object is a wrapper over a set of hash
 refs, where all the hash refs in the set have the same number of elements
@@ -232,28 +236,28 @@ Of course, like any generic tool, Set::Relation should be widely applicable
 in many different situations.
 
 Now, another situation where you may not want to use Set::Relation is when
-its sibling project L<Muldis Rosetta|Muldis::Rosetta> would serve you
+its sibling project L<Muldis::D::RefEng> would serve you
 better.  In contrast to Set::Relation, which is standalone and intended to
-integrate closely with Perl, Muldis Rosetta implements a whole programming
+integrate closely with Perl, Muldis::D::RefEng implements a whole programming
 language distinct from Perl, L<Muldis D|Muldis::D>, and presents a superior
 environment at large for working with the relational model of data; you use
 it sort of like how you use L<DBI> to talk to a SQL DBMS, as a separate
-thing walled off from Perl.  The benefits of using Muldis Rosetta over
+thing walled off from Perl.  The benefits of using Muldis::D::RefEng over
 Set::Relation are multiple, including much better performance and
 scalability, and that it can directly persist data as you'd expect a DBMS
 to do, as well as provide easy access to many other relational model
 features like stronger typing and arbitrary database constraints, and
 nested transactions, as well as access to full powered DBMS engines like
 PostgreSQL and Oracle and SQLite (though you don't have to use those and
-Muldis Rosetta can be used purely implemented in Perl).
+Muldis::D::RefEng can be used purely implemented in Perl).
 
 I<That all said, Set::Relation is actually implemented and works today,
-while Muldis Rosetta is still under construction and you can't use it yet.>
+while Muldis::D::RefEng is still under construction and you can't use it yet.>
 
 That brings out another important reason why Set::Relation exists now; it
-also serves as a proof of concept for a main part of Muldis D and Muldis
-Rosetta, or for a so-called "truly relational DBMSs" in general.  It
-demonstrates ideal features and behaviour for relational operators, in a
+also serves as a proof of concept for a main part of Muldis D and
+Muldis::D::RefEng, or for a so-called "truly relational DBMSs" in general.
+It demonstrates ideal features and behaviour for relational operators, in a
 functioning form that users can experiment with right now.  Set::Relation
 is also meant to serve as inspiration for similar projects, and better
 illustrate features that would be nice for modern programming languages to
@@ -272,7 +276,7 @@ measure their effect and know we didn't break the behaviour.  Also if the
 behaviour/API is nailed first, then new benchmarks we make along the way
 will be backwards compatible with previous baseline versions.  But keep in
 mind that only some kinds of improvements are suitable; some others may be
-inordinately complex and may better be relegated to Muldis Rosetta or other
+inordinately complex and may better be relegated to Muldis::D::RefEng or other
 overlapping solutions instead; the distinction can be discussed
 case-by-case as solutions are proposed.>
 
@@ -1762,9 +1766,9 @@ These Perl 5 packages that are in the current distribution are classes that
 implement the Set::Relation roles: L<Set::Relation::V1>,
 L<Set::Relation::V2>.
 
-These other Perl 6 packages: L<Muldis::Rosetta>, L<Set>.
+These other Perl 6 packages: L<Muldis::D::RefEng>, L<Set>.
 
-These other Perl 5 packages: L<Muldis::Rosetta>, L<Set::Object>,
+These other Perl 5 packages: L<Muldis::D::RefEng>, L<Set::Object>,
 L<Set::Scalar>.
 
 =head1 BUGS AND LIMITATIONS
@@ -1831,9 +1835,9 @@ providing files for the test suite, and giving other constructive input.
 
 Several public email-based forums exist whose main topic is
 the L<Muldis D|Muldis::D> language and its implementations, especially
-the L<Muldis Rosetta|Muldis::Rosetta> reference implementation, but also
+the L<Muldis::D::RefEng> reference implementation, but also
 the L<Set::Relation> module.  They exist so that users of Muldis D or
-Muldis Rosetta can help each other, or so that help coming from the
+Muldis::D::RefEng can help each other, or so that help coming from the
 projects' developers can be said once to many people, rather than
 necessarily to each individually.  All of these you can reach via
 L<http://mm.darrenduncan.net/mailman/listinfo>; go there to manage your
@@ -1843,18 +1847,18 @@ subscriptions to, or view the archives of, the following:
 
 =item C<muldis-db-announce@mm.darrenduncan.net>
 
-This low-volume list is mainly for official announcements from Muldis D or
-Muldis Rosetta developers, though developers of related projects can also
-post their announcements here.  This is not a discussion list.
+This low-volume list is mainly for official announcements from Muldis D
+language or implementation developers, though developers of related projects
+can also post their announcements here.  This is not a discussion list.
 
 =item C<muldis-db-users@mm.darrenduncan.net>
 
 This list is for general discussion among people who are using Muldis D or
-any of its implementations, especially the Muldis Rosetta reference
+any of its implementations, especially the Muldis::D::RefEng reference
 implementation.  This is the best place to ask for basic help in getting
-Muldis Rosetta installed on your machine or to make it do what you want.
+Muldis::D::RefEng installed on your machine or to make it do what you want.
 If you are in doubt on which list to use, then use this one by default.
-You could also submit feature requests for Muldis Rosetta or report
+You could also submit feature requests for Muldis D projects or report
 perceived bugs here, if you don't want to use CPAN's RT system.
 
 =item C<muldis-d-language@mm.darrenduncan.net>
@@ -1868,8 +1872,8 @@ is it the place for non-implementers to get help in using said.
 =item C<muldis-db-devel@mm.darrenduncan.net>
 
 This list is for discussion among people who are designing or implementing
-the Muldis Rosetta DBMS framework core, or who are implementing Muldis
-Rosetta Engines, or who are writing Muldis Rosetta core documentation,
+Muldis::D::RefEng, or other Muldis D implementations,
+or who are writing Muldis::D::RefEng core documentation,
 tests, or examples.  It is not the main forum for the Muldis D language
 itself, nor is it the place for non-implementers to get help in using said.
 
@@ -1879,7 +1883,7 @@ An official IRC channel for Muldis D and its implementations is also
 intended, but not yet started.
 
 Alternately, you can purchase more advanced commercial support for various
-Muldis D implementations, particularly Muldis Rosetta, from its author by
+Muldis D implementations, particularly Muldis::D::RefEng, from its author by
 way of Muldis Data Systems; see L<http://www.muldis.com/> for details.
 
 =cut
